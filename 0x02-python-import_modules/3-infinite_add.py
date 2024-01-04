65a818890dc0cb0bee6ec6b4cc9d5aa47d7b0835
youssef-elmelegy <youssefelmelegy999@gmail,com>
2024-01-04 07:52:10 -0800

    add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,5 +2,7 @@
 if __name__ = "__main__":
     import sys
 
-    to = sum(int(x) for x in sys.argv[1:])
-    print("{}".format(to))
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
