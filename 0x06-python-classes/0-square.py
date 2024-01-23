8307b4a29e08c3bb86318a78ddd8fa4200fa1981
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-01-23 07:37:02 -0800

    add

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
-"""
-creating a new class.
-"""
+"""Square class module.
 
+This module defines a simple `Square` class.
+
+Example:
+    To create a square:
+
+
+Attributes:
+    side_length (int): Length of each side of the square.
+
+"""
 
 class Square:
-    """our square class."""
+    """A simple class representing a square.
 
+    Args:
+        side_length (int): Length of each side of the square.
+
+    Attributes:
+        side_length (int): Length of each side of the square.
+
+    """
     pass
