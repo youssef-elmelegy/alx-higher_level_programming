fde597507aff39f6032699101f330febb5d13bdc
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-04-08 12:20:30 -0700

    warm_up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 function Factorial (num) {
-        if (num === 0 || num === 1) {
-                return 1;
-        }
-        else {
-                return (num * Factorial (num - 1));
-        }
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return (num * Factorial(num - 1));
+  }
 }
 
-if ((process.argv[2]) != undefined && !isNaN(process.argv[2])) {
-        console.log(Factorial (Number(process.argv[2])));
+if ((process.argv[2]) !== undefined && !isNaN(process.argv[2])) {
+  console.log(Factorial(Number(process.argv[2])));
 } else {
-        console.log(1);
+  console.log(1);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-if ((process.argv[2]) != undefined && !isNaN(process.argv[2])) {
-        for (let y = 0; y < process.argv[2]; y++) {
-                for (let x = 0; x < process.argv[2]; x++){
-                        process.stdout.write('X');
-                }
-                console.log();
-        }
+if ((process.argv[2]) !== undefined && !isNaN(process.argv[2])) {
+  for (let y = 0; y < process.argv[2]; y++) {
+    for (let x = 0; x < process.argv[2]; x++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
 } else {
-        console.log('Missing size');
+  console.log('Missing size');
 }
