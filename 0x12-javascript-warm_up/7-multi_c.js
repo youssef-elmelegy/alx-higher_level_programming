4db5a6ac771caa0334693a47ce6b0bfe6d35e588
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-04-08 11:12:28 -0700

    warm_up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if ((process.argv[2]) && process.argv[2] > -1) {
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log('C is fun');
+     console.log('C is fun');
   }
 } else if (process.argv[2] === undefined){
   console.log('Missing number of occurrences');
