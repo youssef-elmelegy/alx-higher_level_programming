4d4ee459124209f45d1eac7f945278c15700b06f
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-04-26 07:00:04 -0700

    fetch

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""fetch url"""
+
+import urllib.request as rq
+
+
+if __main__ == __name__:
+    """ Python script that fetches https://alx-intranet.hbtn.io/status """
+    req = rq.Request("https://alx-intranet.hbtn.io/status")
+    with rq.urlopen(req) as response:
+        data = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
