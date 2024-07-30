e47ff9e4b4f5bae487f8b6f0d8813fa4712fffbb
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-07-30 05:20:57 -0700

    starwars

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
+const url = process.argv[2];
+
+request(url, (error, response) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
   console.log(`code: ${response.statusCode}`);
 });
